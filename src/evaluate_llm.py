de5d2f228948034a409305f448097baabb54d28f
cracrazjf <cracrazjf@gmail.com>
2025-12-07 23:44:51 -0600

    update

--- src/evaluate_llm.py
+++ src/evaluate_llm.py
@@ -96,9 +96,6 @@ def main():
                         batch_size=cfg.data.batch_size, 
                         shuffle=False, 
                         collate_fn=collate_fn)
-    
-    for batch in loader:
-        print(batch["attention_mask"])
 
     tm.evaluate(loader, eval_fn=eval_fn, epoch=0)
     
